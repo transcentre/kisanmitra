232b1f232ce63ab9bac49088ebcb54bdb1b0db0d
Niranjan P B <niranjanpb03@gmail.com>
2017-01-23 16:56:06 +0530

    initial

---
+++ android_login_api/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "mysql3.gear.host");
+define("DB_USER", "rootroot1");
+define("DB_PASSWORD", "#Basawaraj08");
+define("DB_DATABASE", "kisan");
+?>
